6c26d074d951b2d93275922dc338e30ad05ca92f
Edwin Serna <edwinflow888@gmail.com>
2022-05-14 12:23:05 -0500

    Se agregaron los componentes del shell

---
+++ src/app/shell/right-sidebar/right-sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shell-right-sidebar',
+  templateUrl: './right-sidebar.component.html',
+  styleUrls: ['./right-sidebar.component.scss'],
+})
+export class RightSidebarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -8,9 +8,20 @@ import { I18nModule } from '@app/i18n';
 import { AuthModule } from '@app/auth';
 import { ShellComponent } from './shell.component';
 import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, TranslateModule, NgbModule, AuthModule, I18nModule, RouterModule],
-  declarations: [HeaderComponent, ShellComponent],
+  declarations: [
+    HeaderComponent,
+    ShellComponent,
+    SidebarComponent,
+    TopNavbarComponent,
+    RightSidebarComponent,
+    FooterComponent,
+  ],
 })
 export class ShellModule {}
