72700c967107bcf6c286f41a49bcd252fd93405d
Gabrieluza24 <gabrieluza24@gmail.com>
2022-05-14 15:55:45 -0400

    Incluir Token en interceptor

--- src/app/@shared/http/api-prefix.interceptor.ts
+++ src/app/@shared/http/api-prefix.interceptor.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
 import { environment } from '@env/environment';
+import { Credentials } from '@app/auth/credentials.service';
+
+const credentialsKey = 'credentials';
 
 /**
  * Prefixes all requests not starting with `http[s]` with `environment.serverUrl`.
@@ -11,9 +13,20 @@ import { environment } from '@env/environment';
   providedIn: 'root',
 })
 export class ApiPrefixInterceptor implements HttpInterceptor {
+  private credentials: Credentials | null = null;
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (!/^(http|https):/i.test(request.url)) {
-      request = request.clone({ url: environment.serverUrl + request.url });
+      const savedCredentials = sessionStorage.getItem(credentialsKey) || localStorage.getItem(credentialsKey);
+      if (savedCredentials) {
+        this.credentials = JSON.parse(savedCredentials);
+        request = request.clone({
+          url: environment.serverUrl + request.url,
+          setHeaders: {
+            Authorization: `Bearer ${this.credentials?.token}`,
+          },
+        });
+      }
     }
     return next.handle(request);
   }
