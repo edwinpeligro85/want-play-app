f726a4bb37f8d53ee320b91f006a2dc0bf376254
Edwin Serna <edwinflow888@gmail.com>
2022-05-14 09:57:16 -0500

    Se agregá la Uri de Api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ import { env } from './.env';
 export const environment = {
   production: true,
   version: env['npm_package_version'],
-  serverUrl: 'https://api.chucknorris.io',
+  serverUrl: 'https://want-play-api.herokuapp.com',
   defaultLanguage: 'es-ES',
   supportedLanguages: ['es-ES', 'en-US'],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,7 @@ import { env } from './.env';
 export const environment = {
   production: false,
   version: env['npm_package_version'] + '-dev',
-  serverUrl: 'http://localhost:3500',
+  serverUrl: 'https://want-play-api.herokuapp.com',
   defaultLanguage: 'es-ES',
   supportedLanguages: ['es-ES', 'en-US'],
 };
