683a6abfa40b154bdf0824492a73ba2232f67d58
Camilo <camiloomosqueraa@gmail.com>
2022-05-17 00:02:53 -0500

    Basic header

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '@app/api/models';
+import { Shell } from './shell.service';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss'],
+  //providers: [Shell]
 })
 export class ShellComponent implements OnInit {
-  constructor() {}
+  public user: User | undefined;
 
-  ngOnInit() {}
+  constructor(private _shell: Shell) {}
+
+  ngOnInit() {
+    this.loadUser();
+  }
+
+  loadUser() {
+    this._shell.getMe().subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
--- src/app/shell/shell.service.ts
+++ src/app/shell/shell.service.ts
@@ -2,11 +2,19 @@ import { Routes, Route } from '@angular/router';
 
 import { AuthenticationGuard } from '@app/auth';
 import { ShellComponent } from './shell.component';
-
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from './../api/services';
+import { User } from '@app/api/models';
 /**
  * Provides helper methods to create routes.
  */
+@Injectable({
+  providedIn: 'root',
+})
 export class Shell {
+  constructor(private _auth: AuthService) {}
+
   /**
    * Creates routes using the shell component and authentication.
    * @param routes The routes to add.
@@ -19,5 +27,9 @@ export class Shell {
       children: routes,
       canActivate: [AuthenticationGuard],
     };
+  }
+
+  getMe(): Observable<User> {
+    return this._auth.authControllerGetMe();
   }
 }
